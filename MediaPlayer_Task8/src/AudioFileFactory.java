104c7b5bea20319d7f81aa3c8054535c06c0f51f
Rohan Raj <rohan.raj@nixorcollege.edu.pk>
2024-11-15 12:23:09 +0100

    Test D passed

--- MediaPlayer_Task8/src/AudioFileFactory.java
+++ MediaPlayer_Task8/src/AudioFileFactory.java
@@ -3,14 +3,15 @@ public class AudioFileFactory {
 
 	public static AudioFile createAudioFile(String path) {
 		String extension = path.substring(path.lastIndexOf(".") + 1);
-        switch (extension) {
-            case "wav":
-                return new WavFile(path);
-            case "mp3":
-                return new TaggedFile(path);
-            default:
-                throw new IllegalArgumentException("Unsupported file format");
-        }
+		switch (extension.toLowerCase()) {
+		    case "wav":
+		        return new WavFile(path);
+		    case "mp3":
+		    case "ogg":
+		        return new TaggedFile(path);
+		    default:
+		        throw new IllegalArgumentException("Unknown suffix for AudioFile \"" + path + "\"");
+		}
 	}
 	
 	public static void main(String[] args) {
