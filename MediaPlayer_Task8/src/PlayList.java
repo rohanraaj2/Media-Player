cf17ce4e087468c4f0bfcba98793e7ebc364366d
Rohan Raj <rohan.raj@nixorcollege.edu.pk>
2024-11-15 12:12:49 +0100

    Test C passed

--- MediaPlayer_Task8/src/PlayList.java
+++ MediaPlayer_Task8/src/PlayList.java
@@ -1,5 +1,10 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PlayList {
 	
@@ -41,10 +46,55 @@ public class PlayList {
 	
 	public void loadFromM3U(String pathname) {
 		AudioFile audioFile = AudioFileFactory.createAudioFile(pathname);
+		List<String> lines = new ArrayList<>();
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(new File(pathname));
+			int lineNo = 1;
+			
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String lineWithNumber = String.format("%03d: %s", lineNo++, line);
+				lines.add(lineWithNumber);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				System.out.println("File " + pathname + " read!");
+				scanner.close();	
+			} catch (Exception e) {
+				// ignore; probably because file could not be opened
+			}
+		}
 	}
 	
 	public void saveAsM3U(String pathname) {
+		FileWriter writer = null;
+		String sep = System.getProperty("line.separator");
 		
+		try {
+			writer = new FileWriter(pathname);
+			
+			List<String> lines = new ArrayList<>();
+			for (AudioFile file : listOfAudioFiles) {
+				lines.add(file.getPathname());
+			}
+			
+			for (String line : lines) {
+				writer.write(line + sep);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to write file " + pathname + "!");
+		} finally {
+			try {
+				System.out.println("File " + pathname + " written!");
+				writer.close();
+			} catch (Exception e) {
+				// ignore; probably because file could not be opened	
+			}
+		}
 	}
 	
 	public List<AudioFile> getList(){
