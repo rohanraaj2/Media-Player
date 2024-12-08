5d8ba489ebe8bed8897ec853106b3dbea3eed900
Rohan Raj <rohan.raj@nixorcollege.edu.pk>
2024-12-08 01:52:15 +0100

    Test B passed

--- MediaPlayer_Task10/src/studiplayer/ui/Player.java
+++ MediaPlayer_Task10/src/studiplayer/ui/Player.java
@@ -1,17 +1,18 @@
 package studiplayer.ui;
 
+import java.io.File;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import studiplayer.audio.NotPlayableException;
+import studiplayer.audio.PlayList;
 
 public class Player extends Application {
+	private PlayList playList;
+	private boolean useCertPlayList = false;
+	public static final String DEFAULT_PLAYLIST = "playlists/DefaultPlayList.m3u";
 	
 	public Player() {}
 	
@@ -19,58 +20,38 @@ public class Player extends Application {
 		BorderPane mainPane = new BorderPane();
 		stage.setTitle("Player");
 		
-//		VBox explainBox = new VBox();
-//		explainBox.getChildren().addAll(explain1, explain2, explain3);
-//		mainPane.setTop(explainBox);
-		
-//		// Tip in Center area
-//		FlowPane centerPane = new FlowPane();
-//		Label tipLabel = new Label();
-		
-//		// Set background color using CSS Style
-//		centerPane.setStyle("-fx-background-color: lightblue");
-//		// Bold print
-//		tipLabel.setStyle("-fx-font-weight: bold");
-//		guess(tipLabel);
-//		centerPane.getChildren().add(tipLabel);
-//		FlowPane.setMargin(tipLabel, new Insets(10, 10, 10, 10));
-//		mainPane.setCenter(centerPane);
-		
-//		// User feedback in Bottom area
-//		FlowPane buttonPane = new FlowPane();
-//		Button tooBigButton = new Button("too big");
-//		Button tooSmallButton = new Button("too small");
-//		Button nailedItButton = new Button("you nailed it!");
-		
-		// Event handling using Lambda Expressions
-//		tooBigButton.setOnAction(e -> {
-//			upper = tip;
-//			guess(tipLabel);
-//		});
-//		tooSmallButton.setOnAction(e -> {
-//			lower = tip;
-//			guess(tipLabel);
-//		});
-//		nailedItButton.setOnAction(e -> {
-//			tipLabel.setText("Finally!");
-//			tooBigButton.setDisable(true);
-//			tooSmallButton.setDisable(true);
-//			nailedItButton.setDisable(true);
-//		});
-		
-//		buttonPane.getChildren().addAll(tooBigButton, tooSmallButton, nailedItButton);
-//		buttonPane.setHgap(10);
-//		buttonPane.setAlignment(Pos.CENTER);
-//		FlowPane.setMargin(tooBigButton, new Insets(10, 10, 10, 10));
-//		FlowPane.setMargin(tooSmallButton, new Insets(10, 10, 10, 10));
-//		FlowPane.setMargin(nailedItButton, new Insets(10, 10, 10, 10));
-//		mainPane.setBottom(buttonPane);
+		if (!this.useCertPlayList) {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Select File To Load");
+			File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+            	setPlayList(file.getPath());
+            }
+		} else {
+			setPlayList("playList.cert.m3u");
+		}
 		
 		Scene scene = new Scene(mainPane, 600, 40);
 		stage.setScene(scene);
-		stage.show();		
+		stage.show();
 	}
 
+	public void setUseCertPlayList(boolean value) {
+		this.useCertPlayList = value;
+	}
+	
+	public void setPlayList(String pathname) {
+		this.playList = new PlayList(pathname);
+	}
+	
+	public void loadPlayList(String pathname) throws NotPlayableException {
+		if (pathname == null || pathname.isEmpty()) {
+			this.playList.loadFromM3U(DEFAULT_PLAYLIST);
+		} else {
+			this.playList.loadFromM3U(pathname);	
+		}
+	}
+	
 	public static void main(String[] args) {
 		launch();
 	}
